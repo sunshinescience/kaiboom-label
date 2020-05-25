31063a4ef4095a22f35f249f69dbee8c8c55f7f0
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-05-25 17:11:01 +0100

    moved to painter widget for image

--- app/app.py
+++ app/app.py
@@ -7,10 +7,24 @@ import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
 
-ClickCoords = collections.namedtuple("ClickCoords", "x_widget y_widget x_image y_image")
+class LabeledImage(QtWidgets.QWidget):
+    def __init__(self, img_path):
+        super().__init__()
+        self.image(img_path)
+    
+    def image(self, img_path):
+        self.pixmap = QtGui.QPixmap(str(img_path))
+        self.setGeometry(self.pixmap.rect())
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)        
+        painter.drawPixmap(self.rect(), self.pixmap)
+        #pen = QPen(Qt.red, 3)
+        #painter.setPen(pen)
+        #painter.drawLine(10, 10, self.rect().width() -10 , 10)
 
 
-class MyWidget(QtWidgets.QWidget):
+class LabelWidget(QtWidgets.QWidget):
     def __init__(self, img_dir):
         super().__init__()
 
@@ -23,8 +37,7 @@ class MyWidget(QtWidgets.QWidget):
         self.shaft_button = QtWidgets.QRadioButton("shaft")  # center of shaft
         self.head_button = QtWidgets.QRadioButton("head")   # club head
         self.shaft_button.setChecked(True)
-        self.image_widget = QtWidgets.QLabel(self)
-        self.next_image()        
+        self.image_widget = LabeledImage(next(self.image_iter))
 
         # layout
         self.layout = QtWidgets.QVBoxLayout()
@@ -35,6 +48,7 @@ class MyWidget(QtWidgets.QWidget):
         hbox.addWidget(self.next_button)
         self.layout.addLayout(hbox)
         self.setLayout(self.layout)
+        self.adjust_size()
 
         # interactions
         self.next_button.clicked.connect(self.next_image)
@@ -43,34 +57,38 @@ class MyWidget(QtWidgets.QWidget):
     def image_click(self, event):
         x = event.pos().x()
         y = event.pos().y()
-        # assumption (empiric): image placed at center vertically and left horizontally
-        y_img_start = self.image_widget.height() // 2 - self.pixmap.height() // 2
-        x_img_start = 0
-        x_img = x - x_img_start
-        y_img = y - y_img_start
-        self.apply_click(ClickCoords(x, y, x_img, y_img))
+        self.apply_click(x, y)
     
-    def apply_click(self, click_coords):
-        print(click_coords)
+    def apply_click(self, x, y):
+        xy = (x, y)
         if self.shaft_button.isChecked():
-            print(f"shaft: {click_coords}")
+            print(f"shaft: {xy}")
         elif self.head_button.isChecked():
-            print(f"head: {click_coords}")
+            print(f"head: {xy}")
         else:
             raise NotImplementedError("New keypoint type button?")
+    
+    def adjust_size(self):
+        self.image_widget.update()
+        image_rect = self.image_widget.rect()
+        self_width_min = image_rect.width() * 1.1
+        self_height_min = image_rect.height() * 1.1
+        self_rect = self.rect()
+        self_rect.setWidth(max(self_width_min, self_rect.width()))
+        self_rect.setHeight(max(self_height_min, self_rect.height()))
+        self.setGeometry(self_rect)
 
     def next_image(self):
-        self.pixmap = QtGui.QPixmap(str(next(self.image_iter)))
-        self.image_widget.setPixmap(self.pixmap)
+        self.image_widget.image(next(self.image_iter))
+        self.adjust_size()
         self.shaft_button.setChecked(True)
-
 
 
 def main():
     app = QtWidgets.QApplication([])
 
     img_dir = Path("/home/kaiboom/Pictures/golf")
-    widget = MyWidget(img_dir)
+    widget = LabelWidget(img_dir)
     widget.resize(800, 600)
     widget.show()
 
