7e4663f64c855facef90ba31f5008a2758bef7b2
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-05-27 10:51:53 +0100

    mvp

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import collections
 import glob
+import json
 import logging
 from pathlib import Path
 import sys
+from typing import List
 
 from PySide2 import QtCore, QtWidgets, QtGui
 
+kpt_alpha = 200
 logger = logging.getLogger("app")
 logging.basicConfig(level=logging.INFO)
 
@@ -16,25 +19,25 @@ KeypointPosition = collections.namedtuple("KeypointPosition", "x y")
 
 class LabeledPerson:
     keypoints = {
-        "clubhead": {"color": QtGui.QColor(255, 0, 0, 127), "short": "CH"}, 
-        "shaftcenter": {"color": QtGui.QColor(0, 0, 255, 127), "short": "SC"}, 
-        "ball": {"color": QtGui.QColor(255, 255, 255, 127), "short": "B"}, 
-        "nose": {"color": QtGui.QColor(255, 128, 0, 127), "short": "N"}, 
-        "left_shoulder": {"color": QtGui.QColor(255, 255, 0, 127), "short": "LS"}, 
-        "right_shoulder": {"color": QtGui.QColor(255, 255, 0, 127), "short": "RS"}, 
-        "left_elbow": {"color": QtGui.QColor(0, 255, 255, 127), "short": "LE"}, 
-        "right_elbow": {"color": QtGui.QColor(0, 255, 255, 127), "short": "RE"}, 
-        "left_wrist": {"color": QtGui.QColor(255, 0, 255, 127), "short": "LW"}, 
-        "right_wrist": {"color": QtGui.QColor(255, 0, 255, 127), "short": "RW"}, 
-        "left_hip": {"color": QtGui.QColor(128, 128, 128, 127), "short": "LH"}, 
-        "right_hip": {"color": QtGui.QColor(128, 128, 128, 127), "short": "RH"}, 
-        "left_knee": {"color": QtGui.QColor(0, 0, 0, 127), "short": "LK"}, 
-        "right_knee": {"color": QtGui.QColor(0, 0, 0, 127), "short": "RK"}, 
+        "clubhead": {"color": QtGui.QColor(255, 0, 0, kpt_alpha), "short": "CH"}, 
+        "shaftcenter": {"color": QtGui.QColor(0, 0, 255, kpt_alpha), "short": "SC"}, 
+        "ball": {"color": QtGui.QColor(255, 255, 255, kpt_alpha), "short": "B"}, 
+        "nose": {"color": QtGui.QColor(255, 128, 0, kpt_alpha), "short": "N"}, 
+        "left_shoulder": {"color": QtGui.QColor(255, 255, 0, kpt_alpha), "short": "LS"}, 
+        "right_shoulder": {"color": QtGui.QColor(255, 255, 0, kpt_alpha), "short": "RS"}, 
+        "left_elbow": {"color": QtGui.QColor(0, 255, 255, kpt_alpha), "short": "LE"}, 
+        "right_elbow": {"color": QtGui.QColor(0, 255, 255, kpt_alpha), "short": "RE"}, 
+        "left_wrist": {"color": QtGui.QColor(255, 0, 255, kpt_alpha), "short": "LW"}, 
+        "right_wrist": {"color": QtGui.QColor(255, 0, 255, kpt_alpha), "short": "RW"}, 
+        "left_hip": {"color": QtGui.QColor(128, 128, 128, kpt_alpha), "short": "LH"}, 
+        "right_hip": {"color": QtGui.QColor(128, 128, 128, kpt_alpha), "short": "RH"}, 
+        "left_knee": {"color": QtGui.QColor(0, 0, 0, kpt_alpha), "short": "LK"}, 
+        "right_knee": {"color": QtGui.QColor(0, 0, 0, kpt_alpha), "short": "RK"}, 
     }
     names = list(keypoints.keys())
 
-    def __init__(self):
-        self.labels = {}
+    def __init__(self, labels=None):
+        self.labels = labels or {}
     
     def __len__(self):
         return len(self.labels)
@@ -42,7 +45,7 @@ class LabeledPerson:
     def items(self):
         return self.labels.items()
     
-    def __setitem__(self, name, pos):
+    def __setitem__(self, name: str, pos: KeypointPosition):
         logger.info(f"Label name={name} set to pos={pos}.")
         assert name in self.names, name
         self.labels[name] = pos
@@ -50,37 +53,84 @@ class LabeledPerson:
     def clear(self):
         self.labels.clear()
     
+    def remove(self, name: str):
+        del self.labels[name]
+    
     def to_json(self):
         return {n: [p.x, p.y] for n, p in self.items()}
+    
+    @classmethod
+    def from_json(cls, data):
+        labels = {}
+        for n, p in data.items():
+            assert n in cls.keypoints.keys()
+            labels[n] = KeypointPosition(*p)
+        return cls(labels)
 
 
 class Persons:
-    def __init__(self, labels=None):
-        self.labels = labels
-        if self.labels is None:
-            self.labels = []
+    def __init__(self, persons: List[LabeledPerson]=None):
+        self.persons = persons
+        if self.persons is None:
+            self.persons = []
         self.active_person_idx = None
         if len(self) > 0:
             self.active_person_idx = 0
     
     def __len__(self):
-        return len(self.labels)        
+        return len(self.persons)        
 
     def __iter__(self):
-        return iter(self.labels)
+        return iter(self.persons)
     
     def __getitem__(self, idx):
-        return self.labels[idx]
+        return self.persons[idx]
+    
+    def to_json(self):
+        return [l.to_json() for l in self.persons]
+    
+    @classmethod
+    def from_json(cls, data):
+        persons = [LabeledPerson.from_json(d) for d in data]
+        return cls(persons)
 
     @property
     def active_person(self):
         if self.active_person_idx is None:
             return None
-        return self.labels[self.active_person_idx]
+        return self.persons[self.active_person_idx]
     
-    def new(self):
-        self.labels.append(LabeledPerson())
+    def new(self) -> int:
+        self.persons.append(LabeledPerson())
         self.active_person_idx = len(self) - 1
+        return self.active_person_idx
+    
+    def delete(self, idx):
+        del self.persons[idx]
+        self.active_person_idx = 0
+        if len(self) < 1:
+            self.active_person_idx = None
+
+class Dataset:
+    def __init__(self, data=None):
+        self.data = data or {}
+    
+    def __setitem__(self, img_fpath: str, persons: Persons):
+        self.data[img_fpath] = persons
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def get(self, img_fpath: str):
+        return self.data.get(img_fpath)
+
+    def to_json(self):
+        return {i: p.to_json() for i, p in self.data.items()}
+    
+    @classmethod
+    def from_json(cls, data):
+        data = {i: Persons.from_json(p) for i, p in data.items()}
+        return cls(data)
 
 
 class LabeledImage(QtWidgets.QWidget):
@@ -118,16 +168,27 @@ class LabeledImage(QtWidgets.QWidget):
 
 
 class LabelWidget(QtWidgets.QWidget):
-    def __init__(self, img_dir):
+    def __init__(self, data_dir):
         super().__init__()
 
         # data
-        self.labels = {}
-        self.image_fpaths = list(img_dir.glob("*.png"))
+        self.data_dir = data_dir
+        self.dataset_fpath = Path(data_dir) / "dataset.json"
+        try:
+            self.dataset = Dataset.from_json(json.load(self.dataset_fpath.open()))
+            logger.info(f"Loaded dataset from {self.dataset_fpath} with {len(self.dataset)} labeled images.")
+        except FileNotFoundError:
+            logger.info("Created new dataset.")
+            self.dataset = Dataset()
+        self.image_fpaths = list(self.data_dir.glob("*.png"))
         self.current_image_idx = None
         self.current_image_fpath = None
 
         # elements
+        self.add_person_button = QtWidgets.QPushButton("+")
+        self.delete_person_button = QtWidgets.QPushButton("-")
+        self.pop_button = QtWidgets.QPushButton("pop")
+        self.save_button = QtWidgets.QPushButton("save")
         self.clear_button = QtWidgets.QPushButton("clear")
         self.next_button = QtWidgets.QPushButton("next")
         self.back_button = QtWidgets.QPushButton("back")
@@ -142,31 +203,51 @@ class LabelWidget(QtWidgets.QWidget):
         hbox = QtWidgets.QHBoxLayout()
         for b in self.keypoint_buttons:
             hbox.addWidget(b)
+        hbox.addWidget(self.pop_button)
         hbox.addWidget(self.person_selector)
+        hbox.addWidget(self.add_person_button)
+        hbox.addWidget(self.delete_person_button)
         hbox.addWidget(self.back_button)
         hbox.addWidget(self.next_button)
+        hbox.addWidget(self.save_button)
         hbox.addWidget(self.clear_button)
         self.layout.addLayout(hbox)
         self.setLayout(self.layout)
 
         # interactions
+        self.add_person_button.clicked.connect(self.add_person)
+        self.delete_person_button.clicked.connect(self.delete_person)
         self.clear_button.clicked.connect(self.clear)
+        self.pop_button.clicked.connect(self.pop)
         self.next_button.clicked.connect(self.next_image)
         self.back_button.clicked.connect(self.last_image)
+        self.save_button.clicked.connect(self.save_dataset)
         self.image_widget.mousePressEvent = self.image_click
     
-    def select_next_kpt_button(self):
-        inext = None
-        for i, b in enumerate(self.keypoint_buttons):
-            if b.isChecked():
-                inext = i + 1
-                if inext == len(self.keypoint_buttons):
-                    inext = 0
+    def save_dataset(self):
+        self.save_current_labels()
+        json.dump(self.dataset.to_json(), self.dataset_fpath.open("w"))
+        logger.info(f"Saved dataset with {len(self.dataset)} labeled images to {self.dataset_fpath}.")
+    
+    def select_next_kpt_button(self, i=None):
+        if i is not None:
+            inext = i
+        else:
+            inext = None
+            for i, b in enumerate(self.keypoint_buttons):
+                if b.isChecked():
+                    inext = i + 1
+                    if inext == len(self.keypoint_buttons):
+                        inext = 0
         self.keypoint_buttons[inext].setChecked(True)
 
     def image_click(self, event):
+        if self.image_widget.persons.active_person_idx is None:
+            logger.error(f"No person selected.")
+            return
         x = event.pos().x()
         y = event.pos().y()
+        # TODO if outside of img dim return
         pos = KeypointPosition(x, y)    
         for b in self.keypoint_buttons:
             if b.isChecked():
@@ -174,13 +255,40 @@ class LabelWidget(QtWidgets.QWidget):
         self.update_image_widget()
         self.select_next_kpt_button()
     
+    def add_person(self, event):
+        idx = self.image_widget.persons.new()
+        self.refresh_person_selector()
+        self.person_selector.setCurrentIndex(idx)
+        self.select_next_kpt_button(0)
+    
+    def delete_person(self, event):
+        idx = self.person_selector.currentIndex()
+        logger.info(f"Deleting person {idx}")
+        self.image_widget.persons.delete(idx)
+        self.person_selector.setCurrentIndex(0)
+        self.update_image_widget()
+        self.refresh_person_selector()
+    
     def clear(self):
         self.image_widget.persons.active_person.clear()
         self.update_image_widget()
         self.keypoint_buttons[0].setChecked(True)
     
+    def pop(self, event):
+        iprev = None
+        for i, b in enumerate(self.keypoint_buttons):
+            if b.isChecked():
+                iprev = max(0, i - 1)
+        try:
+            self.image_widget.persons.active_person.remove(self.keypoint_buttons[iprev].text())
+        except KeyError as e:
+            logger.warning(f"Can't remove {e}")
+        self.keypoint_buttons[iprev].setChecked(True)
+        self.update_image_widget()
+    
     def update_image_widget(self):
         self.image_widget.update()
+        return  # resizing this way makes it jump to 0,0, leave it to manual
         image_rect = self.image_widget.image_rect
         self_width_min = image_rect.width() + 45
         self_height_min = image_rect.height() + 45
@@ -188,28 +296,35 @@ class LabelWidget(QtWidgets.QWidget):
         self_rect.setWidth(max(self_width_min, self_rect.width()))
         self_rect.setHeight(max(self_height_min, self_rect.height()))
         self.setGeometry(self_rect)
-
-    def jump_image(self, jump):
+    
+    def refresh_person_selector(self):
+        self.person_selector.clear()
+        self.person_selector.addItems([str(i) for i in range(len(self.image_widget.persons))])
+    
+    def save_current_labels(self):
         if self.current_image_fpath is not None:
             labeled_persons = [p for p in self.image_widget.persons if len(p) > 0]
             num_labeled_persons = len(labeled_persons)
             if num_labeled_persons > 0:
-                logger.info(f"Adding {num_labeled_persons} labeled persons for {self.current_image_fpath} to cache.")
-                self.labels[self.current_image_fpath] = Persons(labeled_persons)
+                image_fname = self.current_image_fpath.name
+                logger.info(f"Adding {num_labeled_persons} labeled persons for {image_fname} to cache.")
+                self.dataset[image_fname] = Persons(labeled_persons)
+
+    def jump_image(self, jump):
+        self.save_current_labels()
         if self.current_image_idx is None:
             self.current_image_idx = 0
         else:
             self.current_image_idx += jump
         self.current_image_fpath = self.image_fpaths[self.current_image_idx]
-        persons = self.labels.get(self.current_image_fpath)
+        persons = self.dataset.get(self.current_image_fpath.name)
         if self.image_widget is None:
             self.image_widget = LabeledImage(self.current_image_fpath, persons=persons)
         else:
             self.image_widget.new(self.current_image_fpath, persons=persons)
         if len(self.image_widget.persons) < 1:
             self.image_widget.persons.new()
-        self.person_selector.clear()
-        self.person_selector.addItems([str(i) for i in range(len(self.image_widget.persons))])
+        self.refresh_person_selector()
         self.update_image_widget()
         self.keypoint_buttons[0].setChecked(True)
     
